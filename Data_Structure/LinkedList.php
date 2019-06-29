56a3b46959fe4c01c3e687d6bb4d51565ea0d120
rahil93 <rahilstar11@gmail.com>
2019-06-29 12:03:25 +0530

    Linked List insert impl

--- Data_Structure/LinkedList.php
+++ Data_Structure/LinkedList.php
@@ -1,29 +1,54 @@
 <?php
 // require 'Node.php';
-class LinkedList{
-  public Node $head;
-  class Node{
-    $data;
-    $Node next;
+class Node{
+  public $data;
+  public $next;
+  function __construct($data)
+  {
+    $this->data = $data;
   }
+}
+
+class LinkedList{
+  public static $head;
+
   public function insert($data)
   {
-    Node $node = new Node();
-    $node->$data = $data;
-    $node->next = null;
+    $node = new Node($data);
 
-    if ($head == null) {
-      $head = $node;
+    if (self::$head == null) {
+      self::$head = $node;
     }
 
-    Node $n = $head;
+    $n = self::$head;
     while ($n->next != null) {
       $n = $n->next;
     }
     $n->next = $node;
+    $node->next = null;
+    // $node = &$node;
   }
 
-
+  public function display()
+  {
+    $n = self::$head;
+    while ($n->next != null) {
+      echo "|".$n->data."|-->";
+      $n = $n->next;
+    }
+    echo "|".$n->data."|null";
+  }
 }
+
+  $data;
+  $size;
+  echo "Enter the no. of node add in Linked list : ";
+  fscanf(STDIN,"%s\n",$size);
+  for ($i = 0; $i < $size; $i++) {
+    fscanf(STDIN,"%s\n",$data);
+    LinkedList::insert($data);
+  }
+
+  LinkedList::display();
 
  ?>
