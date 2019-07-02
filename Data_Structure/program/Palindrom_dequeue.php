0df7cd6d1136f2e5cd7fbe12868a43fe7df68e1f
rahil93 <rahilstar11@gmail.com>
2019-07-02 21:38:18 +0530

    Implemented Palindrome stack

---
+++ Data_Structure/program/Palindrom_dequeue.php
@@ -0,0 +1,27 @@
+<?php
+
+require '../Dequeue/Dequeue.php';
+require '../../Algorithm/Utility.php';
+
+$str;
+echo "Enter String : ";
+fscanf(STDIN,"%s\n",$str);
+for ($i = 0; $i < strlen($str); $i++) {
+  Dequeue::addRear($str[$i]);
+}
+Dequeue::display();
+
+$front;
+$rear;
+for ($i = 0; $i < strlen($str) / 2; $i++) {
+  $front = Dequeue::removeFront();
+  $rear = Dequeue::removeRear();
+  if ($front != $rear) {
+    echo "$str is not a Palindrome...";
+    return false;
+  }
+  echo "$str is Palindrome";
+  return true;
+}
+
+ ?>
