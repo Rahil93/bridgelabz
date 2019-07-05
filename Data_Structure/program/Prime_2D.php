56ac687306d8441624725034f9cc32a673fe997a
rahil93 <rahilstar11@gmail.com>
2019-07-05 16:33:37 +0530

    modified Prime 2D

--- Data_Structure/program/Prime_2D.php
+++ Data_Structure/program/Prime_2D.php
@@ -12,66 +12,49 @@ function prime()
     }
     return $arr;
 }
-$arr = prime();
-// var_dump($arr);
 
-$k = 0;
-$range = 100;
-$index = 0;
-while ($range < 1000) {
-    for ($i=0; $i < 10; $i++) { 
-        $temparr = [];
-        $carr = [];
-        for ($j=0; $j < 25; $j++) { 
-            if ($arr[$k] < $range) {
-                $temparr[$j] = $arr[$k]; 
-                if ($k <= sizeof($arr) - 2) {
-                    $k++;
+function clac()
+{
+    $arr = prime();
+
+    $k = 0;
+    $range = 100;
+    $index = 0;
+    while ($range < 1000) {
+        for ($i=0; $i < 10; $i++) { 
+            $temparr = [];
+            $carr = [];
+            for ($j=0; $j < 25; $j++) { 
+                if ($arr[$k] < $range) {
+                    $temparr[$j] = $arr[$k]; 
+                    if ($k <= sizeof($arr) - 2) {
+                        $k++;
+                    }
+                    
                 }
-                
+                else {
+                    $temparr[$j] = 0;
+                    
+                }
             }
-            else {
-                $temparr[$j] = 0;
-                
-            }
+            $carr = $temparr;
+            darr($carr,$index);
+            $range = $range + 100;
+            $index++;
         }
-        $carr = $temparr;
-       
-        darr($carr,$index);
-        $range = $range + 100;
-        echo $index++;
     }
 }
+
 
 
 function darr($arr,$index)
 
-{
-    $darr = [[]];
-    // var_dump();
-
-    // for ($i=$index; $i <= $index; $i++) { 
-        for ($j=0; $j < 25; $j++) { 
-              $darr[$index][$j] = $arr[$j]." ";
-        }
-        echo "\n";
-    // }
-    // var_dump($darr);
-
+{   
+    $arr1 = array($index);
+    $arr2 = array_fill_keys($arr1,$arr);
+    print_r($arr2);
 }
 
-    function initArray()
-    {
-        $arr = [];
-        for ($i = 0; $i < 6; $i++) {
-            $array1 = array();
-            for ($j = 0; $j < 7; $j++) {
-                $array1[$j] = 0;
-            }
-            array_push($arr, $array1);
-        }
-        // var_dump($arr);
-        return $arr;
-    }
+    clac();
 
 ?>
