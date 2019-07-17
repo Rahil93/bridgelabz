d47fa65bdc795f6ed541d047dd979697d6320dc0
rahil93 <rahilstar11@gmail.com>
2019-07-17 12:14:12 +0530

    Modified singleton lazy design pattern

--- Design_Pattern/Singleton_Lazy.php
+++ Design_Pattern/Singleton_Lazy.php
@@ -1,6 +1,6 @@
-<?php
+ <?php
 
-class Singleton_Lazy
+final class Singleton_Lazy
 { 
     private static $instance;
     private static $counter = 0;
@@ -18,11 +18,28 @@ class Singleton_Lazy
                 return self::$instance;
             }   
     }
+
+    // using this nested class without using final keyword for class it created an another object for a class which destroy the sigleto concept.
+    // public function inner_class()
+    // {
+    //     return new class() extends Singleton_Lazy{
+    //         private $instance;
+
+    //         public static function getInstance()
+    //         {
+    //             self::$instance = new Singleton_Lazy();
+    //             return self::$instance;
+    //         }
+    //     };
+    // }
+    
 }
 
 $objStudent = Singleton_Lazy::getInstance();
 echo "Object for Student\n";
 $objEmployee = Singleton_Lazy::getInstance();
 echo "Object for Employee\n";
+// $objinner = Singleton_Lazy::inner_class();
+// echo "Object of inner class\n";
 
 ?>
