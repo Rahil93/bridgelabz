188df1797f9ffa31141174da51fd1377e43df9a5
rahil93 <rahilstar11@gmail.com>
2019-07-17 14:41:23 +0530

    Comment added to Factory Pattern

--- Design_Pattern/Factory_Pattern.php
+++ Design_Pattern/Factory_Pattern.php
@@ -1,19 +1,27 @@
 <?php
 
+// abstract computer class
 abstract class Computer
 {
-    public abstract function getRam();
+    // abstract method that defined in a another class which extends this class
+    public abstract function getRam(); 
     public abstract function getHDD();
     public abstract function getCPU();
+
+    public function toString()
+    {
+        return "Ram : ".$this->getRam()." HDD : ".$this->getHDD()." CPU : ".$this->getCPU()."\n";
+    }
 }
 
+// class PC extends Computer class now this class will defined all abstract method
 class PC extends Computer
 {
     private $RAM;
     private $HDD;
     private $CPU;
 
-    public function __contruct($RAM,$HDD,$CPU)
+    public function __construct($RAM,$HDD,$CPU)
     {
         $this->RAM = $RAM;
         $this->HDD = $HDD;
@@ -22,27 +30,28 @@ class PC extends Computer
 
     public function getRam()
     {
-        return $this->$RAM;
+        return $this->RAM;
     }
 
     public function getHDD()
     {
-        return $this->$HDD;
+        return $this->HDD;
     }
 
     public function getCPU()
     {
-        return $this->$CPU;
+        return $this->CPU;
     }
 }
 
+// class Server extends Computer class now this class will defined all abstract method
 class Server extends Computer
 {
     private $RAM;
     private $HDD;
     private $CPU;
 
-    public function __contruct($RAM,$HDD,$CPU)
+    public function __construct($RAM,$HDD,$CPU)
     {
         $this->RAM = $RAM;
         $this->HDD = $HDD;
@@ -51,20 +60,21 @@ class Server extends Computer
 
     public function getRam()
     {
-        return $this->$RAM;
+        return $this->RAM;
     }
 
     public function getHDD()
     {
-        return $this->$HDD;
+        return $this->HDD;
     }
 
     public function getCPU()
     {
-        return $this->$CPU;
+        return $this->CPU;
     }
 }
 
+// Computer Factory Class produce Server class & Computer Class
 class ComputerFactory
 {
     public static function getComputer($Type,$RAM,$HDD,$CPU)
@@ -79,9 +89,16 @@ class ComputerFactory
     }
 }
 
-$objpc = ComputerFactory::getComputer("pc","8","50","2.4");
+//Creating pc & server object from Computer factory Class
+$objpc = ComputerFactory::getComputer("pc","8 GB","50 TB","2.4 GHz");
 $objserver = ComputerFactory::getComputer("server","16 GB","1 TB","3.2 GHz");
 
+//Displaying PC products
+echo "PC Configuration : ".$objpc->toString();
+// Displaying Server Products.
+echo "Server Configuration : ".$objserver->toString();
+
+// U can access above class properties using Reflection 
 $ref = new ReflectionObject($objpc);
 print_r($ref->getName());
 print_r($ref->getProperties());
